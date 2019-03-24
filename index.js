1c722f756b660ca21d2b55bb5c45f91ebffbbeb6
Melissa Real <melissa.real@hotmail.com>
2019-03-24 18:12:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+function lowerCaseDrivers(arrayTo){
+  const maps = arrayTo.map(x => x.toLowerCase() );
+  return maps;
+}
+
+function nameToAttributes(names){
+ const newObj = { names.map(firstName,lastName) };
+
+}
+
+function attributesToPhrase(){
+
+}
